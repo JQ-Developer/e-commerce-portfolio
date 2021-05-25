6abb0857a77e1b36e55b5db3f3b82a29c4cf3301
José Quintero <jqv.developer@gmail.com>
2021-05-24 22:57:14 -0400

    updated shop data with  new data, updated cart utils to group items on cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,36 @@
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
-  </div>
-);
+//Importing the action
+import { addItem } from "../../redux/cart/cart.actions";
 
-export default CollectionItem;
+const CollectionItem = ({ id, item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        ADD TO CART
+      </CustomButton>
+    </div>
+  );
+};
+
+//Esta funcion devuleve un objeto que lo que hace es tomar un item y pasarselo a la accion que luego será evluada en el redux y si todo es correcto se despacha al store, para que el componente se actualice
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
